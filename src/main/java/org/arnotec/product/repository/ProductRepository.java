56672f46cd7ad37fdb3e82eddff45d40748121e3
Arnaud <tsofackarnaud@gmail.com>
2021-09-28 21:20:36 +0100

    Product Micro service with @RepositoryRestController (Spring Data Rest)

--- src/main/java/org/arnotec/product/repository/ProductRepository.java
+++ src/main/java/org/arnotec/product/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package org.arnotec.product.repository;
 
 import org.arnotec.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+@RepositoryRestController
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
