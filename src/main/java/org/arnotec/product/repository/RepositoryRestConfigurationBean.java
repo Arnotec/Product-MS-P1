afe1d675c6178cb500da7f2be18ef6c3f3bb8ea2
Arnaud <tsofackarnaud@gmail.com>
2021-09-29 16:32:38 +0100

    Adding RepositoryRestConfiguration to expose Ids in responses using Spring Data Rest.

---
+++ src/main/java/org/arnotec/product/repository/RepositoryRestConfigurationBean.java
@@ -0,0 +1,15 @@
+package org.arnotec.product.repository;
+
+import org.arnotec.product.entity.Product;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@Configuration
+public class RepositoryRestConfigurationBean {
+    private RepositoryRestConfiguration restConfiguration;
+
+    public RepositoryRestConfigurationBean(RepositoryRestConfiguration restConfiguration) {
+        this.restConfiguration = restConfiguration;
+        this.restConfiguration.exposeIdsFor(Product.class);
+    }
+}
